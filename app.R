1338acbf7e9ef50a55aa60d8784f43685669ee9d
Barbara Borges Ribeiro <barb.b.ribeiro@gmail.com>
2016-10-14 17:00:16 +0100

    beautiful!

--- app.R
+++ app.R
@@ -17,33 +17,41 @@ server <- function(input, output, session) {
   oldSort$n <- 0
   
   rv <- reactiveValues(
-    data = data
+    data = data,
+    selected = NULL
   )
   
   observe({
     rv$data <- rv$data %>% filter(Title %in% input$flt)
-    rv$data$yaxis <- 
-      switch(input$yaxis,
-             Repos = rv$data$Repos,
-             Stars = rv$data$Stars,
-             Followers = rv$data$Followers,
-             Following = rv$data$Following,
-             Contributions = rv$data$Contributions)
+    rv$data$yaxis <- switch(input$yaxis,
+                            Repos = rv$data$Repos,
+                            Stars = rv$data$Stars,
+                            Followers = rv$data$Followers,
+                            Following = rv$data$Following,
+                            Contributions = rv$data$Contributions)
     if (input$sort != oldSort$n) {
+      username <- rv$data$GitHubUsername[rv$selected]
+      print(username)
       rv$data <- rv$data %>% arrange(yaxis)
+      rv$selected <- which(rv$data$GitHubUsername == username)
       rv$data$GitHubUsername <- factor(rv$data$GitHubUsername, 
                                        levels = rv$data$GitHubUsername)
       oldSort$n <- input$sort
+      # rv$selected <- NULL
     }
     if (!is.null(input$plt_hover$x)) {
-      #rv$data$Color <- "#9d2"
-      selected <- round(input$plt_hover$x)
-      rv$data[selected, "Color"] <- "#ddd"
+      #rv$data$Color <- rep("#9d2", nrow(rv$data))
+      rv$selected <- round(input$plt_hover$x)
+      #rv$data[selected, "Color"] <- "#ddd"
+    # } else {
+      #rv$selected <- NULL
     }
   })
   
   output$plt <- renderPlot({
-    ggplot(rv$data, aes(x = GitHubUsername, y = yaxis, fill = Color)) + 
+    data <- rv$data
+    data[rv$selected, "Color"] <- "#ddd"
+    ggplot(data, aes(x = GitHubUsername, y = yaxis, fill = Color)) + 
       geom_bar(stat="identity")
   })
 }
