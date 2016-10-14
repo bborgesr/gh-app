c67daca9fc176008eeaad3179617e6bc7af11416
Barbara Borges Ribeiro <barb.b.ribeiro@gmail.com>
2016-10-14 16:27:25 +0100

    still working

--- app.R
+++ app.R
@@ -20,7 +20,6 @@ server <- function(input, output, session) {
     data = data
   )
   
-  #selectedData <- reactive({
   observe({
     data <- data %>% filter(Title %in% input$flt)
     data$yaxis <- 
@@ -30,59 +29,25 @@ server <- function(input, output, session) {
              Followers = data$Followers,
              Following = data$Following,
              Contributions = data$Contributions)
-    #data <- data %>% arrange(yaxis)
-    #data$GitHubUsername <- factor(data$GitHubUsername, levels <- data$GitHubUsername)
-    # if (!is.null(input$plt_hover$x)) {
-    #   selected <- round(input$plt_hover$x)
-    #   data[selected, "Color"] <- "#ddd"
-    # }
-    #data$GitHubUsername <- factor(data$GitHubUsername, levels <- data$GitHubUsername)
-    
-    # data
     rv$data <- data
   })
   
-  sortedData <- reactive({
-    # input$sort
-    #data <- (selectedData())
+  observe({
     data <- rv$data
     if (input$sort != oldSort$n) {
       data <- data %>% arrange(yaxis)
       data$GitHubUsername <- factor(data$GitHubUsername, levels <- data$GitHubUsername)
       oldSort$n <- input$sort
+    }
       if (!is.null(input$plt_hover$x)) {
         selected <- round(input$plt_hover$x)
         data[selected, "Color"] <- "#ddd"
       }
       rv$data <- data
-      return(data)
-    } else {
-      if (!is.null(input$plt_hover$x)) {
-        selected <- round(input$plt_hover$x)
-        data[selected, "Color"] <- "#ddd"
-      }
-      return(data)
-    }
   })
   
-  
-  # hover <- {
-  #   data$GitHubUsername <- factor(data$GitHubUsername, levels <- data$GitHubUsername)
-  #   if (!is.null(input$plt_hover$x)) {
-  #     selected <- round(input$plt_hover$x)
-  #     data[selected, "Color"] <- "#ddd"
-  #   }
-  # }
-  # 
-  # observeEvent(input$sort, {
-  #   data <- data %>% arrange(selectedData()$yaxis)
-  #   data$GitHubUsername <- factor(data$GitHubUsername, levels <- data$GitHubUsername)
-  # })
-  
   output$plt <- renderPlot({
-    # print(selectedData()$GitHubUsername)
-    # print(as.numeric(selectedData()$GitHubUsername))
-    ggplot(sortedData(), aes(x = GitHubUsername, y = yaxis, fill = Color)) + 
+    ggplot(rv$data, aes(x = GitHubUsername, y = yaxis, fill = Color)) + 
       geom_bar(stat="identity")
   })
 }
