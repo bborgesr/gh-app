ef0fcc8a4475c810547adfcb6b868b0d8416b40e
Barbara Borges Ribeiro <barb.b.ribeiro@gmail.com>
2016-10-14 13:25:27 +0100

    say what

--- app.R
+++ app.R
@@ -1,5 +1,6 @@
 
 data$GitHubUsername <- as.factor(data$GitHubUsername)
+data$Color <-  rep("#9d2", nrow(data))
 
 # Define UI
 ui <- fluidPage(
@@ -11,7 +12,6 @@ ui <- fluidPage(
   selectInput("yaxis", "Select the y-axis", 
               choices = c("Repos", "Stars", "Followers", "Following", "Contributions"),
               selected = "Contributions"),
-  actionButton("sort", "Sort"),
   plotOutput("plt", hover = hoverOpts(id = "plt_hover"))
 )
 
@@ -20,18 +20,22 @@ server <- function(input, output, session) {
   
   selectedData <- reactive({
     data <- data %>% filter(Title %in% input$flt)
-    yaxis <-
+    yaxis <- 
       switch(isolate(input$yaxis),
              Repos = data$Repos,
              Stars = data$Stars,
              Followers = data$Followers,
              Following = data$Following,
              Contributions = data$Contributions)
+    if (!is.null(input$plt_hover$x)) {
+      selected <- round(input$plt_hover$x)
+      data[selected, Color] <- "#ddd"
+    }
     data %>% arrange(yaxis)
   })
   
   yaxis <- reactive({
-    switch(isolate(input$yaxis),
+    switch(input$yaxis,
            Repos = selectedData()$Repos,
            Stars = selectedData()$Stars,
            Followers = selectedData()$Followers,
@@ -39,27 +43,8 @@ server <- function(input, output, session) {
            Contributions = selectedData()$Contributions)
   })
   
-  xaxis <- reactive({
-    input$sort
-    factor(selectedData()$GitHubUsername, 
-           levels = selectedData()$GitHubUsername[order(isolate(yaxis()))])
-  })
-  
-  color <- reactive({
-    rep("#9d2", nrow(selectedData()))
-  })
-  
   output$plt <- renderPlot({
-    colors <- color()
-    if (!is.null(input$plt_hover$x)) {
-      selected <- round(input$plt_hover$x)
-      print(as.numeric(xaxis()))
-      print((xaxis()))
-      print((xaxis()[selected]))
-      colors[xaxis()[selected]] <- "#ddd"
-    }
-    print(colors)
-    ggplot(selectedData(), aes(x = xaxis(), y = yaxis(), fill = colors)) + 
+    ggplot(selectedData(), aes(x = GitHubUsername, y = yaxis(), fill = Color)) + 
       geom_bar(stat="identity")
   })
 }
